54f819044fd7436cfdcb4785686d042ddb9b7021
SJKZ1-2565 <seefuture13@gmail.com>
2025-11-12 12:31:51 +0700

    Init

---
+++ src/client/java/com/sjkz1/TornElementalsClient.java
@@ -0,0 +1,10 @@
+package com.sjkz1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TornElementalsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/sjkz1/TornElementalsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.sjkz1;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TornElementalsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
